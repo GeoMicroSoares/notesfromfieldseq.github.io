48d1fac6220b5276ba0ed69d15169fa54c9c73f9
Nicholas Loman <n.j.loman@bham.ac.uk>
2017-01-14 20:06:10 +0000

    only do fast5 files

--- scripts/remove-analyses-block-from-basecalls.py
+++ scripts/remove-analyses-block-from-basecalls.py
@@ -10,11 +10,12 @@ if len(sys.argv) != 2:
 count = 0
 for root, dirs, files in os.walk(sys.argv[1]):
 	for fn in files:
-    		filepath = root + '/' + fn
-		fh = h5py.File(filepath, 'r+')
-		if 'Analyses' in fh:
-			del fh['Analyses']
-			count += 1
-		fh.close()
+		if fn.endswith('.fast5'):
+			filepath = root + '/' + fn
+			fh = h5py.File(filepath, 'r+')
+			if 'Analyses' in fh:
+				del fh['Analyses']
+				count += 1
+			fh.close()
 
 print >>sys.stderr, "Removed %d analyses blocks" % (count)
