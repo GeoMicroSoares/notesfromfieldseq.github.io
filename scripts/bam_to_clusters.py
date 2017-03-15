1e5e8deef5bbf70a6906251d6fe4d2857a295388
Nick Loman <n.j.loman@bham.ac.uk>
2017-03-15 16:12:16 +0000

    refseq curated loci

--- scripts/bam_to_clusters.py
+++ scripts/bam_to_clusters.py
@@ -2,34 +2,58 @@ import pysam
 import sys
 from collections import defaultdict
 
-THRESHOLD = 100
-
 def write_reference(align):
 	fh = open('references/' + align.reference_name.replace('|','_') + '.fasta', 'w')
 	fh.write(">%s\n%s\n" % (align.reference_name, align.get_reference_sequence()))
 	fh.close()
 
-filecache = {}
-numseqs = defaultdict(int)
+def checkalign(alignment, args):
+	if alignment.unmapped:
+		return 0
 
-infile = pysam.AlignmentFile(sys.argv[1], "rb")
-for s in infile:
-	if s.is_unmapped:
-		continue
+	if args.min_length:
+		if alignment.reference_length < args.min_length:
+			return 0
 
-	numseqs[s.reference_name] += 1
+	if args.max_length:
+		if alignment.reference_length > args.max_length:
+			return 0
+	return 1
 
-infile = pysam.AlignmentFile(sys.argv[1], "rb")
-for s in infile:
-	if s.is_unmapped:
-		continue
+def make_clusters(args):
+	filecache = {}
+	numseqs = defaultdict(int)
 
-	if numseqs[s.reference_name] > THRESHOLD:
-		if s.reference_name not in filecache:
-			filecache[s.reference_name] = open('clusters/' + s.reference_name + '.cluster.fasta', 'w')
-			write_reference(s)
+	infile = pysam.AlignmentFile(args.bamfile, "rb")
+	for s in infile:
+		if not checkalign(s, args):
+			continue
 
-		filecache[s.reference_name].write(">" + s.query_name + "\n" + s.query + "\n")
+		numseqs[s.reference_name] += 1
 
-for k, v in numseqs.iteritems():
-	print "%s\t%s" % (k, v)
+	infile = pysam.AlignmentFile(args.bamfile, "rb")
+	for s in infile:
+		if not checkalign(s, args):
+			continue
+
+		if numseqs[s.reference_name] > args.min_reads:
+			if s.reference_name not in filecache:
+				filecache[s.reference_name] = open('clusters/' + s.reference_name + '.cluster.fasta', 'w')
+				write_reference(s)
+
+			filecache[s.reference_name].write(">" + s.query_name + "\n" + s.query + "\n")
+
+	for k, v in numseqs.iteritems():
+		print "%s\t%s" % (k, v)
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Parse SAM/BAM file into reference clusters.')
+parser.add_argument('--min-length', dest='min_length', type=int, help='minimum length of the alignment')
+parser.add_argument('--max-length', dest='max_length', type=int, help='maximum length of the alignment')
+parser.add_argument('bamfile', type=str, help='sam/bam file')
+parser.add_argument('min_reads', type=int, help='minimum number of reads to form a cluster')
+
+args = parser.parse_args()
+print args
+make_clusters(args)
